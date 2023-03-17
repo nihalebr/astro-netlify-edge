aba57f82a3d37036a673a08915e0e00dd6eb5ef7
Ebrahim Nihal <nihal2019ebr@gmail.com>
2023-03-17 23:24:52 +0530

    Update geolocation.ts

--- netlify/edge-functions/geolocation.ts
+++ netlify/edge-functions/geolocation.ts
@@ -10,7 +10,7 @@ export default async (request: Request, context: Context) => {
   const regex = /COUNTRYNAME/i;
 
   // Replace the content
-  const countryName = context.geo?.country?.name || "somewhere in the world";
+  const countryName = context || "somewhere in the world";
 
   const updatedPage = page.replace(regex, countryName);
   return new Response(updatedPage, response);
